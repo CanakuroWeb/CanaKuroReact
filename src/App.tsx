2cc8089f50a71cb2bcd24a8904972f6f0144e129
KuroVine9 <kurovine9@gmail.com>
2023-03-21 21:09:47 +0900

    카드추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import "./amuirum"
+import Amuirum from "./amuirum";
 
-function App() {
+function App(){
   const [count, setCount] = useState(0)
   return (
     <div className="App">
@@ -27,6 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+        <Amuirum/>
     </div>
   )
 }
---
+++ src/amuirum.tsx
@@ -0,0 +1,21 @@
+function amuirum(){
+
+    return(
+        <div id="card">
+            <div id="name" className="cat">
+                <div className="key">이름 :</div>
+                <div className="val">Kuro9</div>
+            </div>
+            <div id="hakbun" className="cat">
+                <div className="key">학번 :</div>
+                <div className="val">2171227</div>
+            </div>
+            <div id="hobby" className="cat">
+                <div className="key">취미 :</div>
+                <div className="val">null</div>
+            </div>
+        </div>
+    )
+}
+
+export default amuirum
