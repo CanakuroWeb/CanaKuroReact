302c96bdac897c9e40013b895453f8e32e42848e
KuroVine9 <kurovine9@gmail.com>
2023-03-23 12:52:18 +0900

    fix: input시 렌더링 수정

--- src/todo.tsx
+++ src/todo.tsx
@@ -1,30 +1,36 @@
-import {useContext, useState} from "react";
+import {useContext, useEffect, useRef, useState} from "react";
 import {DarkModeContext, useDarkDispatch} from "./DarkModeState";
 
 function Todo() {
     const [list, setList] = useState<{ id: number, value: string }[]>([])
-    const [input, setInput] = useState<string>("")
+    const input = useRef<HTMLInputElement>(null)
 
     const {mode} = useContext(DarkModeContext)
     const dispatch = useDarkDispatch()
     console.log(mode)
     // 왜 콘솔로그 두개씩?
-    // input state 바뀔때마다 콘솔로그찍히는ㄴ거보면 onChange하면 리로드되는거같은데 안좋은상황인듯?
+    // input state 바뀔때마다 콘솔로그찍히는ㄴ거보면 onChange하면 리로드되는거같은데 안좋은상황인듯? -> 고침
     // useMemo 쓰면 되는가? 일단잘거임몰라
+    // webstorm 커밋은되는데 푸시 안눌려서 콘솔로 하는중
+
+    useEffect(() => {
+        input.current?.focus()
+    }, [])
+
+    const saveInput = () => {
+        if (input.current) {
+            setList([{id: list.length + 1, value: input.current.value}, ...list])
+            input.current.value = ""
+            input.current.focus()
+        }
+    } // function이랑 람다에 이름붙이는거 차이??
 
     return (
         <div className="todo-place">
             <div className="todo-input">
-                <input value={input} onChange={(e) => setInput(e.currentTarget.value)}
+                <input ref={input}
                        placeholder="무슨 일이 일어나고 있나요?"/>
-                <button onClick={() => {
-                    if (input !== "") {
-                        setList(() => {
-                            return [{id: list.length + 1, value: input}, ...list]
-                        });
-                        setInput("")
-                    }
-                }}>
+                <button onClick={saveInput}>
                     확인
                 </button>
             </div>
